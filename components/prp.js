1294a537c9464874fe58f03f1522766fd3230a71
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2021-12-10 05:24:34 +0100

    prp

--- components/prp.js
+++ components/prp.js
@@ -6,7 +6,7 @@ const prp = () => {
         <section id="privacy-policy" className="py-20 pt-20 px-10 lg:mt-16">
             <div className="relative mt-2 lg:mt-6">
                 <div className="sm:w-2/4 lg:w-5/12 mx-auto px-2 mb-20">
-                    <h1 className="text-3xl text-center bg-gradient-to-r from-purple-900 to-purple-600 text-transparent bg-clip-text opacity-80">
+                    <h1 className="text-3xl text-center bg-gradient-to-r from-purple-900 to-purple-600 text-transparent bg-clip-text opacity-90">
                         Privacy Policy
                     </h1>
 
@@ -15,28 +15,28 @@ const prp = () => {
                     <Image className=" mb-16  items-center  mx-auto" src={Prpi} alt="" width="1460" height="730" priority />
 
 
-                    <h1 className="mt-4 mb-10 flex text-lg text-center text-white opacity-80 ">
+                    <h1 className="mt-4 mb-10 flex text-lg text-center text-white opacity-90 ">
                         Personal Data processed for the following purposes :</h1>
 
                     <ul className="list-outside list-disc">
-                        <li className="mt-4 flex text-lg text-left text-purple-900 opacity-80 ">
+                        <li className="mt-4 flex text-lg text-left text-purple-900 opacity-90 ">
                             Contacting the User:
 
                         </li>
-                        <li className="mt-4 flex text-lg text-left text-white opacity-50 ">Personal Data: email address, first name, last name, phone number</li>
+                        <li className="mt-4 flex text-lg text-left text-white opacity-90 ">Personal Data: email address, first name, last name, phone number</li>
 
 
-                        <li className="mt-4 flex text-lg text-left text-purple-900 opacity-80 ">
+                        <li className="mt-4 flex text-lg text-left text-purple-900 opacity-90 ">
                             Managing data collection and online surveys:
                         </li>
-                        <li className="mt-4 flex text-lg text-left text-white opacity-50 ">Personal Data: email address, first name, last name</li>
+                        <li className="mt-4 flex text-lg text-left text-white opacity-90 ">Personal Data: email address, first name, last name</li>
 
-                        <li className="mt-4 flex text-lg text-left text-purple-900 opacity-80 ">
+                        <li className="mt-4 flex text-lg text-left text-purple-900 opacity-90 ">
                             Interaction with external social networks and platforms:
                             Buttons and social media widgets
 
                         </li>
-                        <li className="mt-4 flex text-lg text-left text-white opacity-50 ">Personal Data: Tracker, Usage Data</li>
+                        <li className="mt-4 flex text-lg text-left text-white opacity-90 ">Personal Data: Tracker, Usage Data</li>
 
 
                     </ul>
