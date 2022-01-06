226bfb42b6f9263b98fcf9d192c2f088b38048aa
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-01-06 04:25:56 +0100

    lint update

--- components/Dev.js
+++ components/Dev.js
@@ -55,7 +55,7 @@ const Dev = () => {
                     <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">1</div>
                     <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
                         <motion.div whileInView={{ scale: 0.7 }} transition={{ duration: .7 }} className="flex-shrink-0 w-24 h-24 bg-gray-800 text-purple-400 rounded-full inline-flex items-center justify-center">
-                            <svg fill="none" stroke="currentColor" stroke-Linecap="round" stroke-Linejoin="round" stroke-width="2" className="w-12 h-12" viewBox="0 0 24 24">
+                            <svg fill="none" stroke="currentColor" stroke-Linecap="round" stroke-Linejoin="round" stroke-Width="2" className="w-12 h-12" viewBox="0 0 24 24">
                                 <path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
 							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
 							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
@@ -75,7 +75,7 @@ const Dev = () => {
                     <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">2</div>
                     <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
                         <motion.div whileInView={{ scale: 0.7 }} transition={{ duration: .7 }} className="flex-shrink-0 w-24 h-24 bg-gray-800 text-purple-400 rounded-full inline-flex items-center justify-center">
-                            <svg fill="none" stroke="currentColor" stroke-Linecap="round" stroke-Linejoin="round" stroke-width="2" className="w-12 h-12" viewBox="0 0 24 24">
+                            <svg fill="none" stroke="currentColor" stroke-Linecap="round" stroke-Linejoin="round" stroke-Width="2" className="w-12 h-12" viewBox="0 0 24 24">
                                 <path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
 							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
 							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
@@ -95,7 +95,7 @@ const Dev = () => {
                     <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-purple-500 text-white relative z-10 title-font font-medium text-sm">3</div>
                     <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
                         <motion.div whileInView={{ scale: 0.7 }} transition={{ duration: .7 }} className="flex-shrink-0 w-24 h-24 bg-gray-800 text-purple-400 rounded-full inline-flex items-center justify-center">
-                            <svg fill="none" stroke="currentColor" stroke-Linecap="round" stroke-Linejoin="round" stroke-width="2" className="w-12 h-12" viewBox="0 0 24 24">
+                            <svg fill="none" stroke="currentColor" stroke-Linecap="round" stroke-Linejoin="round" stroke-Width="2" className="w-12 h-12" viewBox="0 0 24 24">
                                 <path fill="none" d="M7.197,16.963H7.195c-0.204,0-0.399-0.083-0.544-0.227l-6.039-6.082c-0.3-0.302-0.297-0.788,0.003-1.087
 							C0.919,9.266,1.404,9.269,1.702,9.57l5.495,5.536L18.221,4.083c0.301-0.301,0.787-0.301,1.087,0c0.301,0.3,0.301,0.787,0,1.087
 							L7.741,16.738C7.596,16.882,7.401,16.963,7.197,16.963z"></path>
