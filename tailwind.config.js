aff38db59af7557e12927877d115cf7f30611706
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-01-11 20:20:51 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'Karla': ['sans-serif']
+      'Karla': ['Karla', 'sans-serif']
     },
     extend: {},
     container: {
