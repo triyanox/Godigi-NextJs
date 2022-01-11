6aad0d3e15b26ea32f7c26ed9db80237310616e3
Mohamed Achaq <92867709+triyanox@users.noreply.github.com>
2022-01-11 19:54:48 +0100

    Fonts

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,7 @@ class MyDocument extends Document {
                 <Head >
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@300&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
